025a01652a1d69c287b4b8b0bba3dd2772fc2e05
Miyazaki Hiroyuki <JPZ7123@MN01580833.local>
2024-02-25 14:36:09 +0900

    save

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -6,6 +6,7 @@ import posts from './posts';
 import postsTimeline from './postsTimeline';
 import postsTimelineStock from './postsTimelineStock';
 import rank from './rank';
+import rankAll from './rankAll';
 import chDetail from './chDetail';
 // import tunedChs from './tunedChs';
 import tuneCh from './tuneCh';
@@ -13,6 +14,7 @@ import tuneCh from './tuneCh';
 export const reducers = {
   bootOption,
   rank,
+  rankAll,
   tuneCh,
   posts,
   postsTimeline,
--- src/state.ts
+++ src/state.ts
@@ -1,24 +1,25 @@
 import { Types, inits } from '@common/models';
-import { Ch } from '@common/models/Ch';
 import { ReduxState } from '@api/redux/store';
 
 export default class ApiState {
   bootOption: Types['BootOption'];
-  rank: Types['Rank'][];
   tuneCh: Types['Ch'];
-  chDetail: Types['ChDetail'];
+  rank: Types['Rank'][];
+  rankAll: Types['Rank'][];
   posts: Types['Post'][];
   postsTimeline: Types['Post'][];
   postsTimelineStock: Types['Post'][];
+  chDetail: Types['ChDetail'];
   logs: string[];
   constructor(reduxState: Partial<ReduxState> = {}) {
     this.bootOption = reduxState.bootOption || inits.bootOption;
-    this.rank = reduxState.rank || [];
     this.tuneCh = reduxState.tuneCh || inits.ch;
-    this.chDetail = reduxState.chDetail || inits.chDetail;
+    this.rank = reduxState.rank || [];
+    this.rankAll = reduxState.rankAll || [];
     this.posts = reduxState.posts || [];
     this.postsTimeline = reduxState.postsTimeline || [];
     this.postsTimelineStock = reduxState.postsTimelineStock || [];
+    this.chDetail = reduxState.chDetail || inits.chDetail;
     this.logs = [];
   }
 }
